9799e521ad967e747b91248a06502e07b222daa8
Eduardo Garcia Fensterseifer <86329470+EduardoFen12@users.noreply.github.com>
2024-10-23 16:17:05 -0300

    Primeiro commit criando repositório

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify, render_template
+import google.generativeai as genai
+
+app = Flask(__name__)
+
+
+API_KEY = 'coloque_sua_chave_aqui'
+
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+
+with open('./static/docs/arquivo.txt', 'r', encoding='utf-8') as arquivo:
+    
+    CONTEXT = arquivo.read()
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def process_input():
+    # Recebe o input do JavaScript
+    data = request.json
+    userQuestion = data.get('question')
+    
+    fullQuestion = f"Com base no texto a seguir, responda a pergunta:\n{CONTEXT}\nPergunta: {userQuestion}"
+    
+    response = model.generate_content(fullQuestion)
+    
+    response_text = response.candidates[0].content.parts[0].text
+
+    return jsonify({'result': response_text})   
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
